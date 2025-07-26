4d03fd91d1cb51ed402dc187d0897e065b241a66
Jasur <jasurisoqov685@gmail.com>
2025-07-26 13:30:30 +0500

    fix(proxy server)

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://apialfa.apoint.uz/v1',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [react(), visualizer()],
   resolve: {
     alias: {
